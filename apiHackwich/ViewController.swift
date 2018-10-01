540848291520791775396bf9f2c1562974744eb7
DHINGRA,GD GOLDY <rl107147@rlas-macair-14.rlas-116.org>
2018-10-01 12:18:19 -0500

    setup storyboard

--- apiHackwich/ViewController.swift
+++ apiHackwich/ViewController.swift
@@ -1,24 +1,14 @@
-//
-//  ViewController.swift
-//  apiHackwich
-//
-//  Created by DHINGRA,GD GOLDY on 10/1/18.
-//  Copyright © 2018 DHINGRA,GD GOLDY. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class SourcesViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
 
 
 }
